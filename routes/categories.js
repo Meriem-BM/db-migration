fb3322085d51fea1c1204dec0571d33861aa31da
Meriem <barhoumi.meriem1@gmail.com>
2022-04-30 01:53:34 +0000

    commit

--- routes/categories.js
+++ routes/categories.js
@@ -10,24 +10,25 @@ async function clearCategory(){
   for (let i = 0; i < categories.length; i++) {
     let newCategory = {}
     console.log(categories[i]);
+    newCategory.oid = categories[i]._doc._id || ''
     newCategory.name = categories[i]._doc.name || ''
-    newCategory.img = categories[i]._doc.img
-    newCategory.company = categories[i]._doc.description || ''
-    newCategory.approved = false
-    newCategory.created_at = companies[i]._doc.created_at
-    newCategory.updated_at = companies[i]._doc.updated_at
+    newCategory.img = categories[i]._doc.img || ''
+    newCategory.company = categories[i]._doc.company || ''
+    newCategory.approved = categories[i]._doc.approved || false
+    newCategory.created_at = companies[i]._doc.created_at || new Date().toISOString()
+    newCategory.updated_at = companies[i]._doc.updated_at || new Date().toISOString()
     newData.push(newCategory)
   }
   return newData
 }
 
 app.post('/', async (req, res) => {
-  const company = await clearCategory()
-  console.log(company)
+  const category = await clearCategory()
+  console.log(category)
   let insertedData
   await axios.post(process.env.queryInterface, {
     collection: 'categories',
-    data: company
+    data: category
   }).catch(e => {
     console.log(e);
     res.send(e)
